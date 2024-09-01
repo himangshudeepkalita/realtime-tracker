1827fde40d74019572e874faea1021b2aec6f488
itzhimangshu <itzhimangshu@gmail.com>
2024-09-01 13:09:58 +0530

    Setting up static files and folders for CSS and JavaScript

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from "express";
 const app = express();
 
 import { createServer } from "http";
+import path from "path";
 
 import { Server } from "socket.io";
 const server = createServer(app);
 const io = new Server(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("hey");
